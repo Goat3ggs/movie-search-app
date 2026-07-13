4402c3136aa158b391d7d8a3a930edeae59f042e
Stefania <stefpandi12@gmail.com>
2026-07-13 11:44:46 +0300

    feat: create MovieCard static component

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,14 @@
+export default function MovieCard() {
+  return (
+    <div className="card-container">
+      {/* movie 1 */}
+      <div class="movie-card">
+        <img src="https://placeholder.com" alt="Movie 1 Poster" />
+        <div class="movie-info">
+          <div class="movie-title">Cosmic Frontiers</div>
+          <div class="movie-meta">2026 • Sci-Fi</div>
+        </div>
+      </div>
+    </div>
+  );
+}
