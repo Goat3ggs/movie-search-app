1ca74ddaf506a6e234ed1b4b4f6f43aa8ff137f5
Stefania <stefpandi12@gmail.com>
2026-07-13 11:07:17 +0300

    feat: create SearchBar static component

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,16 @@
+export default function SearchBar() {
+  return (
+    <div className="searchbar-container">
+      <form className="searchbar form">
+        <input
+          type="text"
+          className="searchbar__input"
+          placeholder="Type to search"
+        />
+        <button className="searchbar__sbtn" type="submit">
+          Search
+        </button>
+      </form>
+    </div>
+  );
+}
