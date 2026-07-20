6ac694a0dc23a21911629020fd40873f636703bd
Stefania <stefpandi12@gmail.com>
2026-07-20 15:17:39 +0300

    perf: render movie results by displaying API data through components and props

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,12 +1,22 @@
 export default function MovieCard({ movie }) {
+  const baseUrl = "https://image.tmdb.org/t/p/w500" + movie.poster_path;
+  const replaceImg = "/poster-placeholder.svg";
   return (
     <article className="card-container">
       <div className="movie-card">
-        <img src={movie.poster} alt={`${movie.title} poster`} />
+        <img
+          src={movie.poster_path ? baseUrl : replaceImg}
+          alt={`${movie.title ? movie.title : "Untitled "} poster`}
+        />
         <div className="movie-info">
-          <div className="movie-title">{movie.title}</div>
+          <div className="movie-title">
+            {movie.title ? movie.title : "Untitled movie"}
+          </div>
           <div className="movie-meta">
-            {movie.year} • {movie.type}
+            {movie.release_date
+              ? movie.release_date.slice(0, 4)
+              : "No release date"}{" "}
+            • {movie.genre}
           </div>
         </div>
       </div>
